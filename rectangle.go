7531fd28708dabf78a0607fbd4058d2c6ba32dc5
Ai3011 <ilyaanikevich7@gmail.com>
2024-03-16 14:10:41 +0700

    add rectangle

--- rectangle.go
+++ rectangle.go
@@ -1,8 +1,15 @@
 package main
 
-func rectanglePerimeter(a float64, b float64)  float64 {
-	
-	return (a+b)*2
+import "math"
+
+func rectanglePerimeter(a float64, b float64) float64 {
+	return (a + b) * 2
 }
 
-func rectangle
+func rectangleSquare(a float64, b float64) float64 {
+	return a * b
+}
+
+func rectangleDiagonallength(a float64, b float64) float64 {
+	return math.Sqrt(a*a + b*b)
+}
