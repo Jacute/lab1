acb28396144ab490d0b27bdb2aa47dbd2c1f87bb
Ai3011 <ilyaanikevich7@gmail.com>
2024-03-16 15:39:52 +0700

    add triangle in main

--- main.go
+++ main.go
@@ -13,13 +13,17 @@ func menuRectangle() {
 	fmt.Println("===RECTANGLE===\n1. Square\n2. Perimeter\n3. Diagonall Length\n4. Back")
 }
 
+func menuTriangle() {
+	fmt.Println("===Triangle===\n1. Perimeter\n2. Square\n3. Checking for isosceles\n4. Back")
+}
+
 func main() {
 	for {
 		var choice, choice2 int8
 
 		menu()
 		fmt.Print("Choice: ")
-		fmt.Scanf("%d", &choice)
+		fmt.Scan(&choice)
 
 		switch choice {
 		case 1:
@@ -27,15 +31,15 @@ func main() {
 
 			fmt.Println("Write sides of rectangle")
 			fmt.Print("a: ")
-			fmt.Scanf("%f", &a)
+			fmt.Scan(&a)
 			fmt.Print("b: ")
-			fmt.Scanf("%f", &b)
+			fmt.Scan(&b)
 
-		loop:
+		loop1:
 			for {
 				menuRectangle()
 				fmt.Print("Choice: ")
-				fmt.Scanf("%d", &choice2)
+				fmt.Scan(&choice2)
 
 				switch choice2 {
 				case 1:
@@ -45,10 +49,43 @@ func main() {
 				case 3:
 					fmt.Println("Diagonall Length: " + strconv.FormatFloat(rectangleDiagonallength(a, b), 'f', -1, 64))
 				case 4:
-					break loop
+					break loop1
 				}
 			}
+
 		case 2:
+			var a1, b1, c1 float64
+
+			fmt.Println("Write sides of triangle")
+			fmt.Print("a: ")
+			fmt.Scan(&a1)
+			fmt.Print("b: ")
+			fmt.Scan(&b1)
+			fmt.Print("c: ")
+			fmt.Scan(&c1)
+
+		loop2:
+			for {
+				menuTriangle()
+				fmt.Println("Choice: ")
+				fmt.Scan(&choice2)
+
+				switch choice2 {
+				case 1:
+					fmt.Println("Triangle Perimeter: " + strconv.FormatFloat(trianglePerimeter(a1, b1, c1), 'f', -1, 64))
+				case 2:
+					fmt.Println("Triangle Square: " + strconv.FormatFloat(triangleSquare(a1, b1, c1), 'f', -1, 64))
+				case 3:
+					if triangleIsIsosceles(a1, b1, c1) {
+						fmt.Println("Triangle is isosceles")
+					} else {
+						fmt.Println("Triangle is not isosceles")
+					}
+				case 4:
+					break loop2
+				}
+			}
+
 		case 3:
 			return
 		}
