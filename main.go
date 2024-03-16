3d0676d32f8bb774707548ae7726a598d2ab6054
Jacute <you@example.com>
2024-03-16 22:36:59 +0700

    add colors

--- main.go
+++ main.go
@@ -6,15 +6,15 @@ import (
 )
 
 func menu() {
-	fmt.Println("===MENU===\n1. Rectangle\n2. Triangle\n3. Exit")
+	fmt.Println(green("===") + cyan("MENU") + green("===\n") + "1. Rectangle\n2. Triangle\n3. Exit")
 }
 
 func menuRectangle() {
-	fmt.Println("===RECTANGLE===\n1. Square\n2. Perimeter\n3. Diagonall Length\n4. Back")
+	fmt.Println(green("===") + cyan("RECTANGLE") + green("===\n") + "1. Square\n2. Perimeter\n3. Diagonall Length\n4. Back")
 }
 
 func menuTriangle() {
-	fmt.Println("===Triangle===\n1. Perimeter\n2. Square\n3. Checking for isosceles\n4. Back")
+	fmt.Println(green("===") + cyan("Triangle") + green("===\n") + "1. Perimeter\n2. Square\n3. Checking for isosceles\n4. Back")
 }
 
 func main() {
@@ -22,7 +22,7 @@ func main() {
 		var choice, choice2 int8
 
 		menu()
-		fmt.Print("Choice: ")
+		fmt.Print(cyan("Choice: "))
 		fmt.Scan(&choice)
 
 		switch choice {
@@ -38,16 +38,16 @@ func main() {
 		loop1:
 			for {
 				menuRectangle()
-				fmt.Print("Choice: ")
+				fmt.Print(cyan("Choice: "))
 				fmt.Scan(&choice2)
 
 				switch choice2 {
 				case 1:
-					fmt.Println("Rectangle Square: " + strconv.FormatFloat(rectangleSquare(a, b), 'f', -1, 64))
+					fmt.Println("Rectangle Square: " + green(strconv.FormatFloat(rectangleSquare(a, b), 'f', -1, 64)))
 				case 2:
-					fmt.Println("Rectangle Perimeter: " + strconv.FormatFloat(rectanglePerimeter(a, b), 'f', -1, 64))
+					fmt.Println("Rectangle Perimeter: " + green(strconv.FormatFloat(rectanglePerimeter(a, b), 'f', -1, 64)))
 				case 3:
-					fmt.Println("Diagonall Length: " + strconv.FormatFloat(rectangleDiagonallength(a, b), 'f', -1, 64))
+					fmt.Println("Diagonall Length: " + green(strconv.FormatFloat(rectangleDiagonallength(a, b), 'f', -1, 64)))
 				case 4:
 					break loop1
 				}
@@ -65,7 +65,7 @@ func main() {
 			fmt.Scan(&c)
 
 			if !isTriangle(a, b, c) {
-				fmt.Println("There is no triangle with this sides")
+				fmt.Println(red("There is no triangle with this sides"))
 				break
 			}
 
@@ -77,14 +77,14 @@ func main() {
 
 				switch choice2 {
 				case 1:
-					fmt.Println("Triangle Perimeter: " + strconv.FormatFloat(trianglePerimeter(a, b, c), 'f', -1, 64))
+					fmt.Println("Triangle Perimeter: " + green(strconv.FormatFloat(trianglePerimeter(a, b, c), 'f', -1, 64)))
 				case 2:
-					fmt.Println("Triangle Square: " + strconv.FormatFloat(triangleSquare(a, b, c), 'f', -1, 64))
+					fmt.Println("Triangle Square: " + green(strconv.FormatFloat(triangleSquare(a, b, c), 'f', -1, 64)))
 				case 3:
 					if triangleIsIsosceles(a, b, c) {
-						fmt.Println("Triangle is isosceles")
+						fmt.Println(green("Triangle is isosceles"))
 					} else {
-						fmt.Println("Triangle is not isosceles")
+						fmt.Println(red("Triangle is not isosceles"))
 					}
 				case 4:
 					break loop2
