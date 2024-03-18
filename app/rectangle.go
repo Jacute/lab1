f33ff95932020484712f28fa1ff8f5f44782cea7
Ai3011 <ilyaanikevich7@gmail.com>
2024-03-18 15:14:35 +0700

    mod rectangle.go

--- app/rectangle.go
+++ app/rectangle.go
@@ -2,11 +2,11 @@ package main
 
 import "math"
 
-func rectanglePerimeter(a float64, b float64) float64 {
+func rectanglePerimeter(a float64, b float64) float64 { //Периметр
 	return (a + b) * 2
 }
 
-func rectangleSquare(a float64, b float64) float64 {
+func rectangleSquare(a float64, b float64) float64 { // Площадь
 	return a * b
 }
 
